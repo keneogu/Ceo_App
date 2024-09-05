9cd42f4d96d225cdd2e8e1f788d2e13a3744005d
keneogu <kc101com@gmail.com>
2024-09-05 17:08:30 +0100

    update the site wrapper

--- components/wrapper.tsx
+++ components/wrapper.tsx
@@ -5,11 +5,12 @@ interface WrapperProps {
   bg?: string;
   py?: string;
   mt?: string;
+  position?: string;
 }
 
-const Wrapper = ({ children, bg, py, mt }: WrapperProps) => {
+const Wrapper = ({ children, bg, py, mt, position }: WrapperProps) => {
   return (
-    <section className={`w-full ${bg || "bg-white1"} `}>
+    <section className={`w-full ${bg || "bg-white1"} ${position || ""}`}>
       <div
         className={`mx-auto max-w-[1280px] w-[90%] ${
           py || "py-[60px] md:py-[100px]"
