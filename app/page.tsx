34db727c4fb11c82512c70cdea6b6e23dc24f9af
keneogu <kc101com@gmail.com>
2024-09-05 17:09:25 +0100

    add the footer and footerlist component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import Features from "@/components/home/features";
+import Footer from "@/components/home/footer";
+import FrequentQuestions from "@/components/home/frequent-questions";
 import HeroSection from "@/components/home/hero-section";
+import Manage from "@/components/home/manage";
+import Pricing from "@/components/home/pricing";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
@@ -6,6 +11,11 @@ export default function Home() {
     <main className="">
       <Navbar />
       <HeroSection />
+      <Features />
+      <Pricing />
+      <FrequentQuestions />
+      <Manage />
+      <Footer />
     </main>
   );
 }
